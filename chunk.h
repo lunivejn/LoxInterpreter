67e4316abb7d5fcbfcb8a1120dc6bdf4ca577192
lunivejn <dinicmilutin00@gmail.com>
2025-01-17 13:08:11 +0100

    VM barebones

---
+++ chunk.h
@@ -0,0 +1,25 @@
+#ifndef clox_chunk_h
+#define clox_chunk_h
+
+#include "common.h"
+#include "value.h"
+
+typedef enum {
+    OP_CONSTANT,
+    OP_RETURN,
+} OpCode;
+
+typedef struct {
+    uint8_t* code;
+    int count; // used entries
+    int capacity; // number of allocated elements in the array 
+    int* lines;
+    ValueArray constants;
+} Chunk;
+
+void initChunk(Chunk* chunk);
+void freeChunk(Chunk* chunk);
+void writeChunk(Chunk* chunk, uint8_t byte, int line);
+int addConstant(Chunk* chunk, Value value);
+
+#endif
