09a7b2e24487d6a4fde7b9ce277c32379c67e31a
lunivejn <dinicmilutin00@gmail.com>
2025-01-20 13:02:39 +0100

    VM done

---
+++ vm.h
@@ -0,0 +1,28 @@
+#ifndef clox_vm_h
+#define clox_vm_h
+
+#include "chunk.h"
+#include "value.h"
+
+#define STACK_MAX 256
+
+typedef struct {
+    Chunk* chunk;
+    uint8_t* ip;
+    Value stack[STACK_MAX];
+    Value* stackTop;
+} VM;
+
+typedef enum {
+    INTERPET_OK,
+    INTERPET_COMPILE_ERROR,
+    INTERPET_RUNTIME_ERROR
+} InterpretResult;
+
+void initVM();
+void freeVM();
+InterpretResult interpret(Chunk* chunk);
+void push(Value Value);
+Value pop();
+
+#endif
